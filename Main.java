a01fb61c2ce4445027e2fc032363265dcaf30a9e
Isaac Blake <newt1237@gmail.com>
2015-12-09 08:43:57 -0700

    idk

--- Main.java
+++ Main.java
@@ -6,24 +6,6 @@
  */
 public class Main
 {
-     /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+     public static void main(){
+     }
 }
