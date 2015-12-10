b3b9c1bd02398386e75296cd3a333ff3c9806b62
John Chen <jjchen2019@gmail.com>
2015-12-10 10:48:20 -0700

    Added some more code

--- Main.java
+++ Main.java
@@ -21,16 +21,16 @@ public class Main
         String name = kbReader.nextLine();
         System.out.println("Please choose your level:\nEasy\nImpossible insane");
         String level = kbReader.nextLine();
-        if(level.equalsIgnoreCase("insane pro")){
-            System.out.println("You wake up in a cave alone. As you look out the entrance,\n you see a dinosaur. It looks like a little baby.");
-            System.out.println("The dinosaur pounces on you when you just stare at him.\n Game over.");
+        if(level.equalsIgnoreCase("impossible insane")){
+            System.out.println("You wake up in a cave alone. As you look out the entrance,\nyou see a dinosaur. It looks like a little baby.");
+            System.out.println("The dinosaur pounces on you when you just stare at him.\nGame over.");
+            System.exit(0);
         }else if(!level.equalsIgnoreCase("easy")){
             while(!level.equalsIgnoreCase("easy")){
                 System.out.println("That is not a valid level. Please put in a level again.");
             }
         }else{
         }
-        
     }
 
     public static void addScore(int s)
@@ -39,6 +39,16 @@ public class Main
     }
 
     public static void readCommand(String command){
-
+        Scanner typed = new Scanner(System.in);
+        String sentence = typed.nextLine();
+        String[] word = sentence.trim().split(" ");
+    }
+    
+    public static currentRoom(){
+        return
+    }
+    
+    private static void interpretTextRoom(){
+        
     }
 }
