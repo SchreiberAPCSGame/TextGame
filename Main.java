5cecea73c713acbcf424b1459245d5a08eba3a21
John Chen <jjchen2019@gmail.com>
2015-12-10 11:32:01 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main
 {
     public int score;
-    public int health;
+    public int health = 100;
     public Room location;
     public Item currentItem;
     /**
@@ -19,17 +19,22 @@ public class Main
         System.out.println("Enter your name");
         Scanner kbReader = new Scanner(System.in);
         String name = kbReader.nextLine();
-        System.out.println("Please choose your level:\nEasy\nImpossible insane");
+        System.out.println("Please choose your level:\n\tEasy\n\tMedium\n\tHard\n\tInsane\n\tImpossible to Win");
         String level = kbReader.nextLine();
         if(level.equalsIgnoreCase("impossible insane")){
             System.out.println("You wake up in a cave alone. As you look out the entrance,\nyou see a dinosaur. It looks like a little baby.");
             System.out.println("The dinosaur pounces on you when you just stare at him.\nGame over.");
             System.exit(0);
-        }else if(!level.equalsIgnoreCase("easy")){
-            while(!level.equalsIgnoreCase("easy")){
-                System.out.println("That is not a valid level. Please put in a level again.");
-            }
+        }else if(level.equalsIgnoreCase("medium")){
+            this.health = 85;
+        }else if(level.equalsIgnoreCase("hard"))
+            this.health = 65;
+        }else if(level.equalsIgnoreCase("insane"))
+            this.health = 50;
         }else{
+            while(!level.equalsIgnoreCase("easy") | !level.equalsIgnoreCase("medium") || !level.equalsIgnoreCase("hard") || !level.equalsIgnoreCase("insane") || !level.equalsIgnoreCase("impossible to win"){
+                System.out.println("That is not a valid level. Please put in a level again.");
+            
         }
     }
 
